4b7f59c339238ff0f90475d148e83a02badaf2e4
rd1073 <rd1073@srmist.edu.in>
2023-11-23 02:19:27 +0530

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
  
 import './App.css';
 import Login from './components/Login';
+import Navbartop from './components/Navbar';
 import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Navbartop />
        
     </div>
   );
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+
+const Navbartop = () => {
+  return (
+    <div>
+        <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Secure Website</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: Mark Otto
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Profile</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Change Password</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Tasks</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+           
+        </Nav.Item>
+      </Nav>
+      
+    </div>
+  )
+}
+
+export default Navbartop;
