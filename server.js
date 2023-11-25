0e43a572f45703a19075874218955013b37964e8
rd1073 <rd1073@srmist.edu.in>
2023-11-25 20:09:24 +0530

    server updates

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const cors = require("cors");
 const { User}=require("./db")
-
+//const userRoutes=require("./routes/userRoutes")
 
 
 const app = express();
+app.use(express.json());
+app.use(
+    cors({
+      origin: "http://localhost:3000",
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      // <-- location of the react app were connecting to
+      credentials: true,
+    })
+  );
+
+
+//app.use("/api/user", userRoutes);
 
 
 app.listen(5000,console.log(`Server running on 5000`));
